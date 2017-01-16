0681e2c5e8dce331987ec1fae7e0d7ef4bf9c889
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-01-16 14:21:30 -0600

    put the real photos in the cards and made the grid 4X4.

--- js/products.js
+++ js/products.js
@@ -6,49 +6,49 @@ const products = [
         name: "Product1",
         description: "Spicy jalapeno bacon ipsum dolor amet tongue shankle tenderloin, meatloaf turkey shank flank swine beef. Shank swine pastrami chicken ground round pork loin. Turducken meatball alcatra, sirloin beef ribs pork drumstick kielbasa corned beef chicken chuck andouille capicola. ",
         price: "$9,999.99",
-        url: "images/goatee1.jpg"
+        url: "images/cat1.jpg"
     },
     {
         name: "Product2",
         description: "Spicy jalapeno bacon ipsum dolor amet tongue shankle tenderloin, meatloaf turkey shank flank swine beef. Shank swine pastrami chicken ground round pork loin. Turducken meatball alcatra, sirloin beef ribs pork drumstick kielbasa corned beef chicken chuck andouille capicola. ",
         price: "$9,999.99",
-        url: "images/grandpa3.jpg"
+        url: "images/cat2.jpg"
     },
     {
         name: "Product3",
         description: "Spicy jalapeno bacon ipsum dolor amet tongue shankle tenderloin, meatloaf turkey shank flank swine beef. Shank swine pastrami chicken ground round pork loin. Turducken meatball alcatra, sirloin beef ribs pork drumstick kielbasa corned beef chicken chuck andouille capicola. ",
         price: "$9,999.99",
-        url: "images/goatastic4.jpg"
+        url: "images/cat3.jpg"
     },
     {
         name: "Product4",
         description: "Spicy jalapeno bacon ipsum dolor amet tongue shankle tenderloin, meatloaf turkey shank flank swine beef. Shank swine pastrami chicken ground round pork loin. Turducken meatball alcatra, sirloin beef ribs pork drumstick kielbasa corned beef chicken chuck andouille capicola. ",
         price: "$9,999.99",
-        url: "images/gypsy5.jpg"
+        url: "images/cat4.jpg"
     },
     {
         name: "Product5",
         description: "Spicy jalapeno bacon ipsum dolor amet tongue shankle tenderloin, meatloaf turkey shank flank swine beef. Shank swine pastrami chicken ground round pork loin. Turducken meatball alcatra, sirloin beef ribs pork drumstick kielbasa corned beef chicken chuck andouille capicola. ",
         price: "$9,999.99",
-        url: "images/greatgoat6.jpg"
+        url: "images/animal1.jpg"
     },
     {
         name: "Product6",
         description: "Spicy jalapeno bacon ipsum dolor amet tongue shankle tenderloin, meatloaf turkey shank flank swine beef. Shank swine pastrami chicken ground round pork loin. Turducken meatball alcatra, sirloin beef ribs pork drumstick kielbasa corned beef chicken chuck andouille capicola. ",
         price: "$9,999.99",
-        url: "images/gitter7.jpg"
+        url: "images/cat5.jpg"
     },
     {
         name: "Product7",
         description: "Spicy jalapeno bacon ipsum dolor amet tongue shankle tenderloin, meatloaf turkey shank flank swine beef. Shank swine pastrami chicken ground round pork loin. Turducken meatball alcatra, sirloin beef ribs pork drumstick kielbasa corned beef chicken chuck andouille capicola. ",
         price: "$9,999.99",
-        url: "images/grassy8.jpg"
+        url: "images/cat6.jpg"
     },
     {
         name: "Product8",
         description: "Spicy jalapeno bacon ipsum dolor amet tongue shankle tenderloin, meatloaf turkey shank flank swine beef. Shank swine pastrami chicken ground round pork loin. Turducken meatball alcatra, sirloin beef ribs pork drumstick kielbasa corned beef chicken chuck andouille capicola. ",
         price: "$9,999.99",
-        url: "images/grassman9.jpg"
+        url: "images/cat7.jpg"
     }
 ];
 
